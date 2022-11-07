e432bb54d18331cf686dc8542293b20a64324949
alisina79 <116415437+alisina79@users.noreply.github.com>
2022-11-07 17:42:09 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+new = Flask(__name__)
+
+@new.route('/')
+def home():
+    return render_template('home.html')
+
+@new.route('/about')
+def about():
+    return render_template('about.html')
+
+@new.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@new.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == '__main__':
+    new.run(debug=True)
